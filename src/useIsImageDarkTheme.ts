a53ce82add46dda05703f8096ee24b734ad2df79
JA <juanalvarez@cecotec.es>
2024-11-18 17:35:02 +0100

    feat: add hook contrast

---
+++ src/useIsImageDarkTheme.ts
@@ -0,0 +1,53 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+type Return = {
+  imageData: ImageData;
+  width: number;
+  height: number;
+};
+
+export function useIsImageDarkTheme() {
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  const calculateConstrast = useRef<(() => void) | null>(null);
+  const [isOnLoadCalled, setIsOnLoadCalled] = useState(false);
+  const [isDark, setIsDark] = useState(false);
+
+  const setOnLoadImage = useCallback((onLoadImage: () => Return) => {
+    calculateConstrast.current = async () => {
+      const { width, height, imageData } = onLoadImage();
+      const { ContrastCalculator } = await import('./ContrastCalculator');
+      const constrastCalculator = new ContrastCalculator({ width, height });
+      const isDark = constrastCalculator.isDarkTheme(imageData);
+      setIsDark(isDark);
+    };
+    setIsOnLoadCalled((prev) => !prev);
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && calculateConstrast.current) {
+          calculateConstrast.current();
+          observer.disconnect();
+        }
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1,
+      }
+    );
+
+    if (imageRef.current) {
+      observer.observe(imageRef.current);
+    }
+
+    return () => {
+      if (imageRef.current) {
+        observer.unobserve(imageRef.current);
+      }
+    };
+  }, [isOnLoadCalled]);
+
+  return { imageRef, setOnLoadImage, isDark };
+}
